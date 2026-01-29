801718f88e47a480fe03138e5df043bda507fc59
Ezekiel8807 <Ayeoluwaseyi@gmail.com>
2026-01-29 15:58:08 +0100

    follow comments to create proect functionalityy

--- index.js
+++ index.js
@@ -27,16 +27,3 @@
 // - Clear Completed Button
 
 // 5. Initial Load
-
-const inputBox = document.getElementById('taskInput');
-const submitBtn = document.getElementById('addBtn');
-
-const inputValue = inputBox.value;
-
-console.log(inputValue);
-
-// function addTask() {
-//     console.log(inputValue);
-// }
-
-submitBtn.addEventListener('click', addTask);
