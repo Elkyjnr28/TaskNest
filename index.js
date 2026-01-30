ca4178fe6440774f3cde75af616618c17497004e
Elky_Jnr <sophiaelky92@gmail.com>
2026-01-30 18:12:06 +0100

    Updated functions

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 // 1. Select DOM Elements
+const inputBox = document.getElementById('taskInput');
+const submitBtn = document.getElementById('addbtn');
+const total = document.getElementById('totalCount');
+const completed = document.getElementById('completedCount');
+const taskList = document.getElementById('tasksList');
+const clearBtn = document.getElementById('clearBtn');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
 // 2. Initialize State
+let tasks = [];
+let currentFilter = 'all';
 
 // 3. Define Core Functions
 
 // - Function: renderTasks()
+function renderTasks() {
+    taskList.innerHTML = '';
+}
+
+// filter the 'tasks' array based on 'currentFilter'
+const filteredTasks = taskList.filter(task => {
+    if (currentFilter === 'active') return !task.completed;
+    if (currentFilter === 'completed') return task.completed;
+    return true;
+});
+
+// Loop through the filtered tasks and create HTML 
+if (filteredTasks === 0) {
+    taskList.innerHTML = ``;
+}
 
 // - Function: addTask()
+tasks.push(newTask);
 
 // - Function: toggleTask(id)
 
